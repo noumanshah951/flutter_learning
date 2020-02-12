1e66166f9061680667ccb7e8fecb577bb02c0a08
nouman.shah <nouman.shah@visionx.io>
2020-02-12 16:01:15 +0500

    image loading with_error

--- lib/main.dart
+++ lib/main.dart
@@ -3,9 +3,16 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(
     MaterialApp(
-      home: Center(
-        child: Text('Hello world'),
+        home: Scaffold(
+          backgroundColor: Colors.blueGrey,
+          appBar: AppBar(
+        title: Text('i am rich'),
+        backgroundColor: Colors.blueGrey[800],
       ),
+          body: Image(
+            image: NetworkImage('https://github.com/square/picasso/blob/master/website/static/sample.png'),
+          ),
+    ),
     ),
   );
 }

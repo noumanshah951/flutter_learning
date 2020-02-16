e81adf9931b8223bee38e8b93b471d49a466c15b
nouman.shah <nouman.shah@visionx.io>
2020-02-16 20:19:21 +0500

    done with small practice nothing added

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,14 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(
-    MaterialApp(
-        home: Scaffold(
-          backgroundColor: Colors.blueGrey,
-          appBar: AppBar(
-        title: Text('i am rich'),
-        backgroundColor: Colors.blueGrey[800],
-      ),
-          body: Center(
-            child: Image(
-
-              image: AssetImage('images/diamond.png'),
-            ),
-          ),
-    ),
-    ),
+void main(){
+  runApp(MaterialApp(home :Scaffold(
+    backgroundColor: Colors.blueAccent,
+    appBar: AppBar(title: Text("Practice2"),
+    backgroundColor: Colors.black
+        ),
+    body:Center(child: Image( image: AssetImage('images/diamond.png'),))
+     )
+  )
   );
+
 }

db19b2830b85b10b5cb99e6bd7620845e3b0420a
nouman.shah <nouman.shah@visionx.io>
2020-02-17 22:01:10 +0500

    floating button

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,29 @@
 import 'package:flutter/material.dart';
 
-void main(){
-  runApp(MaterialApp(home :Scaffold(
-    backgroundColor: Colors.blueAccent,
-    appBar: AppBar(title: Text("Practice2"),
-    backgroundColor: Colors.black
-        ),
-    body:Center(child: Image( image: AssetImage('images/diamond.png'),))
-     )
-  )
-  );
+void main() {
+  runApp( MyApp());
+}
 
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+          backgroundColor: Colors.white70,
+          appBar: AppBar(
+            title: Text('Learing F'),
+            backgroundColor: Colors.red,
+          ),
+          body:
+        Text('Number of Donuts')
+        ,
+        floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.red,
+        child: Icon(Icons.add),
+      ),
+
+      ),
+    );
+  }
 }

f297b2e85002eb8312d58bdb79d690e6c1c39f41
nouman.shah <nouman.shah@visionx.io>
2020-02-18 16:20:53 +0500

    column rows testing

--- lib/main.dart
+++ lib/main.dart
@@ -1,29 +1,63 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp( MyApp());
+  runApp(MyApp());
 }
-
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-          backgroundColor: Colors.white70,
-          appBar: AppBar(
-            title: Text('Learing F'),
-            backgroundColor: Colors.red,
-          ),
-          body:
-        Text('Number of Donuts')
-        ,
-        floatingActionButton: FloatingActionButton(
-        backgroundColor: Colors.red,
-        child: Icon(Icons.add),
+        home: Scaffold(
+      backgroundColor: Colors.teal,
+      appBar: AppBar(
+        title: Text('Column'),
+        backgroundColor: Colors.white70,
       ),
+      body: SafeArea(
+        // column by default take as much as screen
+          child: Row(
+//            mainAxisSize:MainAxisSize.max,
+//        verticalDirection:  VerticalDirection.down,
+//        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
 
-      ),
-    );
+        children: <Widget>[
+          //Container go big by default but if there is child it wrap to it
+            Container(
+            width: 0.0,
+            height: double.infinity,
+            ),
+
+          SizedBox( height : 20),
+          Container(
+            width: 100,
+            height: 100,
+            margin: EdgeInsets.all(10),
+            padding: EdgeInsets.all(10),
+            color: Colors.red,
+            child: Center(child: Text('Red')),
+          ),
+          SizedBox( height : 20),
+          Container(
+            width: 100,
+            height: 100,
+            margin: EdgeInsets.all(10),
+            padding: EdgeInsets.all(10),
+            color: Colors.blue,
+            child: Center(child: Text('blue')),
+          ),
+          SizedBox( height : 20),
+          Container(
+            width: 100,
+            height: 100,
+            margin: EdgeInsets.all(10),
+            padding: EdgeInsets.all(10),
+            color: Colors.green,
+            child: Center(child: Text('green')),
+          ),
+        ],
+      )),
+    ));
   }
 }
